d34411315e1af7c67d9b796c756feb507c6af763
Kgor <kevin.gorham@gmail.com>
2019-03-04 22:03:55 -0800

    starting working on add to favorites button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,10 @@
-
-
 // Style the buttons, heading, etc with css + bootstrap
-// use columns+rows+divs to format the gifs + data that is returned
-// add each gif to a card that has a border, gif on top, text below, and margin around it for the next gif 
-// add a div separating the gif search portion with another api search function 
 
+// use columns+rows+divs to format the gifs + data that is returned
+
+// add each gif to a card that has a border, gif on top, text below, and margin around it for the next gif 
+
+// add a div separating the gif search portion with another api search function 
 
 // ### Bonus Goals
 
@@ -41,9 +41,9 @@ function displayButtonInfo() {
     // not sure what this line of code is for so I'm hiding it until it becomes useful
 
 
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&limit=10&rating=g&api_key=E4GmjIzr95bf7cgs50n05QPKhxsZ1ZZh";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&limit=10&rating=pg&api_key=E4GmjIzr95bf7cgs50n05QPKhxsZ1ZZh";
 
-    // Creating an AJAX call for the specific dog breed button being clicked
+   
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -54,22 +54,36 @@ function displayButtonInfo() {
         var giphy = response.data;
 
         for (var i = 0; i < giphy.length; i++) {
-            var dataStill = giphy[i].images.fixed_width_still.url;
-            var dataGif = giphy[i].images.fixed_width.url;
+            var dataStill = giphy[i].images.fixed_height_still.url;
+            var dataGif = giphy[i].images.fixed_height.url;
             var img = $("<img>");
-            var rating = giphy[i].rating;
-            var span = $("<span>");
-            span.append("Rating: " + rating)
             img.attr("data-state", "still");
             img.attr("data-still", dataStill);
             img.attr("data-animate", dataGif);
-            img.attr("class", "gifs");
+            img.attr("class", "gifs card-img-top");
             img.attr("src", dataStill);
-            itemDiv.append(img);
-            itemDiv.append(span);
-            $("#item-view").prepend(itemDiv);
+            var cardDiv = $("<div>");
+            cardDiv.attr("class", "card float-left border border-primary");
+            cardDiv.attr("style", "width: 18rem;");
+            var cardBody = $("<div>");
+            cardBody.attr("class", "card-body");
+            var h = $("<h5>");
+            h.attr("class", "card-title")
+            h.text(giphy[i].title)
+            var p = $("<p>");
+            p.attr("class", "card-text");
+            p.text("Rating: " + giphy[i].rating);
+            var saveBtn = $("<button>");
+            saveBtn.attr("class", "btn btn-outline-primary add-to-favorites");
+            saveBtn.text("Add to Favorites");
+            cardBody.append(h);
+            cardBody.append(p);
+            cardBody.append(saveBtn);
+            cardDiv.append(img);
+            cardDiv.append(cardBody);
+            $("#item-view").prepend(cardDiv);
         };
-
+            
         $(".gifs").on("click", function () {
             var state = $(this).attr("data-state");
             if (state === "still") {
@@ -86,45 +100,34 @@ function displayButtonInfo() {
 
 };
 
-// Function for displaying items data
 function renderButtons() {
-
-    // Deleting the items prior to adding new items
-    // (this is necessary otherwise you will have repeat buttons)
     $("#buttons-view").empty();
-
-    // Looping through the array of items
     for (var i = 0; i < items.length; i++) {
-
-        // Then dynamicaly generating buttons for each items in the array
-        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
         var btn = $("<button>");
-        // Adding a class of items-btn to our button
-        btn.addClass("items-btn");
-        // Adding a data-attribute
+        btn.addClass("items-btn btn btn-info");
         btn.attr("data-name", items[i]);
-        // Providing the initial button text
         btn.text(items[i]);
-        // Adding the button to the buttons-view div
         $("#buttons-view").append(btn);
     }
+};
+// function that will run when add to favorites btn is clicked
+function addToFavorites() {
+    console.log("add to favorites function running");
+
 }
 
-// This function handles events where a items button is clicked
+// click event listener for add to favorites btn 
+$(document).on("click", ".add-to-favorites", addToFavorites);
+// Click event listener for new searches
 $("#add-item-btn").on("click", function (event) {
     event.preventDefault();
-    // This line grabs the input from the textbox
     var item = $("#new-input").val().trim();
-
-    // Adding items from the textbox to our array
     items.push(item);
-
-    // Calling renderButtons which handles the processing of our items array
     renderButtons();
 });
 
 // Adding a click event listener to all elements with a class of "items-btn"
-// $(document).on("click", ".items-btn", displayButtonInfo);
+
 $(document).on("click", ".items-btn", function() {
     searchTerm = $(this).attr("data-name");
     displayButtonInfo();
